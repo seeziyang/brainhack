a83f9c32e9e2547fd87f915007eed0157b0a9500
seeziyang <44959961+seeziyang@users.noreply.github.com>
2020-05-31 22:20:11 +0800

    First commit

---
+++ App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import Home from './Screen/Home.js';
+
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+const Stack = createStackNavigator();
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={Home} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default App;
---
+++ Screen/Home.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+import { View, Text, StyleSheet } from 'react-native';
+
+class Home extends Component {
+  render() {
+    return (<View><Text>Hi</Text></View>);
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  }
+});
+
+export default Home;
