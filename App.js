cf3136a54eec1c08ef0c4b0c12a4ec5745bd5f93
junchern wu <junchern97@hotmail.com>
2020-06-03 18:15:47 +0800

    added some ui

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import { Button, Text, Icon, Footer, FooterTab } from 'native-base';
+import { StyleSheet, View, Image } from 'react-native';
 
 import Locations from './Screen/Locations/Locations.js';
 import LocationDetail from './Screen/Locations/LocationDetail.js';
@@ -37,6 +38,7 @@ const TabBar = props => {
           onPress={() => props.navigation.navigate('Locations')}
         >
           <Icon name="bowtie" />
+          <View style={styles.container} />
           <Text>Locations</Text>
         </Button>
 
@@ -66,7 +68,11 @@ const LocationsStack = createStackNavigator();
 const LocationsStackNavigator = () => {
   return (
     <LocationsStack.Navigator>
-      <LocationsStack.Screen name="Locations" component={Locations} />
+      <LocationsStack.Screen
+        style={styles.header}
+        name="Location"
+        component={Locations}
+      />
       <LocationsStack.Screen name="LocationDetail" component={LocationDetail} />
     </LocationsStack.Navigator>
   );
@@ -93,3 +99,65 @@ const UserStackNavigator = () => {
 };
 
 export default App;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#EBECF4',
+  },
+  header: {
+    paddingTop: 64,
+    paddingBottom: 16,
+    backgroundColor: '#FFF',
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderBottomWidth: 1,
+    borderBottomColor: '#EBECF4',
+    shadowColor: '#454D65',
+    shadowOffset: { height: 5 },
+    shadowRadius: 15,
+    shadowOpacity: 0.2,
+    zIndex: 10,
+  },
+  headerTitle: {
+    fontSize: 20,
+    fontWeight: '500',
+  },
+  feed: {
+    marginHorizontal: 16,
+  },
+  feedItem: {
+    backgroundColor: '#FFF',
+    borderRadius: 5,
+    padding: 8,
+    flexDirection: 'row',
+    marginVertical: 8,
+  },
+  avatar: {
+    width: 36,
+    height: 36,
+    borderRadius: 18,
+    marginRight: 16,
+  },
+  name: {
+    fontSize: 15,
+    fontWeight: '500',
+    color: '#454D65',
+  },
+  timestamp: {
+    fontSize: 11,
+    color: '#C4C6CE',
+    marginTop: 4,
+  },
+  post: {
+    marginTop: 16,
+    fontSize: 14,
+    color: '#838899',
+  },
+  postImage: {
+    width: undefined,
+    height: 150,
+    borderRadius: 5,
+    marginVertical: 16,
+  },
+});
--- Screen/Locations/Locations.js
+++ Screen/Locations/Locations.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { FlatList } from 'react-native';
+import { FlatList, StyleSheet, View, Image } from 'react-native';
 import {
   Container,
   Header,
@@ -41,28 +41,95 @@ export default class Locations extends Component {
     const nav = this.props.navigation;
 
     return (
-      <Container>
-        <Content>
-          <FlatList
-            data={Object.entries(this.state.stores)}
-            renderItem={({ item }) => (
-              <Card>
-                <CardItem
-                  button
-                  onPress={() =>
-                    nav.navigate('LocationDetail', { storeId: item[0] })
-                  }
-                >
-                  <Body>
-                    <Text>{item[1]?.storeInfo?.locName}</Text>
-                  </Body>
-                </CardItem>
-              </Card>
-            )}
-            keyExtractor={item => item[0]}
-          />
-        </Content>
-      </Container>
+      <View style={styles.container}>
+        <FlatList
+          style={styles.feed}
+          data={Object.entries(this.state.stores)}
+          renderItem={({ item }) => (
+            <Card>
+              <CardItem
+                button
+                onPress={() =>
+                  nav.navigate('LocationDetail', { storeId: item[0] })
+                }
+              >
+                <Body>
+                  <Text>{item[1]?.storeInfo?.locName}</Text>
+                  <CardItem cardBody>
+                    <Image
+                      source={{ uri: '{item[1]?.storeInfo?.image}' }}
+                      style={{ height: 200, width: null, flex: 1 }}
+                    />
+                  </CardItem>
+                </Body>
+              </CardItem>
+            </Card>
+          )}
+          keyExtractor={item => item[0]}
+        />
+      </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#EBECF4',
+  },
+  header: {
+    paddingTop: 64,
+    paddingBottom: 16,
+    backgroundColor: '#FFF',
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderBottomWidth: 1,
+    borderBottomColor: '#EBECF4',
+    shadowColor: '#454D65',
+    shadowOffset: { height: 5 },
+    shadowRadius: 15,
+    shadowOpacity: 0.2,
+    zIndex: 10,
+  },
+  headerTitle: {
+    fontSize: 20,
+    fontWeight: '500',
+  },
+  feed: {
+    marginHorizontal: 16,
+  },
+  feedItem: {
+    backgroundColor: '#FFF',
+    borderRadius: 5,
+    padding: 8,
+    flexDirection: 'row',
+    marginVertical: 8,
+  },
+  avatar: {
+    width: 36,
+    height: 36,
+    borderRadius: 18,
+    marginRight: 16,
+  },
+  name: {
+    fontSize: 15,
+    fontWeight: '500',
+    color: '#454D65',
+  },
+  timestamp: {
+    fontSize: 11,
+    color: '#C4C6CE',
+    marginTop: 4,
+  },
+  post: {
+    marginTop: 16,
+    fontSize: 14,
+    color: '#838899',
+  },
+  postImage: {
+    width: undefined,
+    height: 150,
+    borderRadius: 5,
+    marginVertical: 16,
+  },
+});
