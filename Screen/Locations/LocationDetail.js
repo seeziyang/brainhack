75f8662e12870c12b36ff397f082464d14a39c04
seeziyang <44959961+seeziyang@users.noreply.github.com>
2020-06-03 18:52:03 +0800

    Add location detail skeleton

--- Screen/Locations/LocationDetail.js
+++ Screen/Locations/LocationDetail.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { FlatList } from 'react-native';
+import { View, StyleSheet, Dimensions } from 'react-native';
 import {
   Container,
   Header,
@@ -17,13 +17,83 @@ import {
 import database from '@react-native-firebase/database';
 
 export default class LocationDetail extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      store: {},
+    };
+  }
+
+  componentDidMount() {
+    this.listenToStore();
+  }
+
+  listenToStore = () => {
+    const storeId = this.props.route.params.storeId;
+
+    database()
+      .ref(`/stores/${storeId}`)
+      .on('value', snapshot => {
+        console.log(snapshot.val());
+        this.setState({ store: snapshot.val() ?? {} });
+      });
+  };
+
+  joinQueue = () => {};
+
   render() {
-    const nav = this.props.navigation;
+    const { queue, storeInfo } = this.state.store;
+    const { locAddress, locName, locPostalCode, locType } = storeInfo ?? {};
 
     return (
       <Container>
-        <Content />
+        <Content>
+          <Card>
+            <CardItem>
+              <Text>{locName}</Text>
+            </CardItem>
+
+            <CardItem>
+              <View>
+                <Text>{locType}</Text>
+                <Text>{locAddress}</Text>
+                <Text>{locPostalCode}</Text>
+              </View>
+            </CardItem>
+          </Card>
+
+          <Button
+            rounded
+            large
+            success
+            iconLeft
+            onPress={this.joinQueue}
+            style={styles.queueButton}
+          >
+            <Icon
+              ios="ios-walk"
+              android="md-walk"
+              style={styles.queueButtonText}
+            />
+            <Text style={styles.queueButtonText}>Queue</Text>
+          </Button>
+        </Content>
       </Container>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  queueButton: {
+    margin: 10,
+    width: Dimensions.get('window').width / 2,
+    justifyContent: 'center',
+    alignSelf: 'center',
+  },
+  queueButtonText: {
+    fontSize: 30,
+    fontWeight: 'bold',
+    color: 'black',
+  },
+});
